721c4f9d44c6e3f929edaa5a95b07b4f2215f155
Daniel Aoyagi <mac_registros@pobox.com>
2023-07-16 11:19:55 -0300

    Retomada

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,78 @@
-import React from "react";
 import "./App.css";
+import React from "react";
 
-import Primeiro from "./components/base/Primeiro";
-import ComParametro from "./components/base/ComParametro";
-import Fragmento from "./components/base/Fragmento";
-import Aleatorio from "./components/base/Aleatorio";
 import Card from "./components/layout/Card";
 
-export default () => (
-  <div className="App">
-    <h1>Fundamentos React</h1>
 
-    <div className="Cards">
-      <Card titulo="#04 - Desafio Aleatório">
-        <Aleatorio min={10} max={20} />
-        <Aleatorio min={1} max={10} />
-      </Card>
-      <Card titulo="#03 - Desafio Fragmento">
-        <Fragmento />
-      </Card>
-      <Card titulo="#02 - Com Parâmetro">
-        <ComParametro
-          titulo="Situação do Aluno"
-          aluno="Pedro"
-          nota={9.3}
-        ></ComParametro>
-      </Card>
-      <Card titulo="#01 - Primeiro Componente">
-        <Primeiro></Primeiro>
-      </Card>
+import IndiretaPai from "./components/comunicacao/IndiretaPai";
+import DiretaPai from "./components/comunicacao/DiretaPai";
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import ParOuImpar from "./components/condicional/ParOuImpar";
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Fragmento from "./components/basicos/Fragmento";
+import ComParametro from "./components/basicos/ComParametro";
+import Primeiro from "./components/basicos/Primeiro";
+
+export default () => (
+    <div className="App">
+        <h1>Fundamentos React</h1>
+
+        <div className="Cards">
+
+            <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+                <IndiretaPai></IndiretaPai>
+            </Card>
+
+            <Card titulo="#09 - Comunicação Direta" color="#59323C">
+                <DiretaPai></DiretaPai>
+            </Card>
+
+            <Card titulo="#08 - Renderização Condicional" color="#982395">
+                <ParOuImpar numero={21}></ParOuImpar>
+                <UsuarioInfo usuario={{ nome: "Fernando" }} />
+                {/* <UsuarioInfo usuario={{}} />
+                <UsuarioInfo /> */}
+            </Card>
+
+            <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+                <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
+            </Card>
+
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Silva">
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
+                </Familia>
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
-  </div>
 );
